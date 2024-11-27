2e9ba46e98d72d1ee9c238cfddaf1c71934719ee
Keaton Lees <keatslees@yahoo.com>
2024-11-27 11:53:13 -0800

    Removed constants temporarily

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,4 +1,4 @@
-import { Wrap } from "./interfaces";
+// import { Wrap } from "./interfaces";
 
 export const baseURL = (
   process.env.NODE_ENV === "production"
@@ -7,18 +7,18 @@ export const baseURL = (
 ) as string;
 
 // TODO: update this
-const defaultBGColor: string = "#FAF9F6";
-const defaultColor: string = "#202020";
-export const newWrap: Wrap = {
-  title: "New Wrap",
-  pages: [
-    {
-      type: "title",
-      bgColor: defaultBGColor,
-      color: defaultColor,
-      content: "Made by Name",
-    },
-    { type: "default", bgColor: defaultBGColor, color: defaultColor },
-    { type: "credits", bgColor: defaultBGColor, color: defaultColor },
-  ],
-};
+// const defaultBGColor: string = "#FAF9F6";
+// const defaultColor: string = "#202020";
+// export const newWrap: Wrap = {
+//   title: "New Wrap",
+//   pages: [
+//     {
+//       type: "title",
+//       bgColor: defaultBGColor,
+//       color: defaultColor,
+//       content: "Made by Name",
+//     },
+//     { type: "default", bgColor: defaultBGColor, color: defaultColor },
+//     { type: "credits", bgColor: defaultBGColor, color: defaultColor },
+//   ],
+// };
